0cce88b26c5a7878a78d71d6e61ccad8f49df7d2
Efraim Munthe <efrayanglain@gmail.com>
2023-04-24 09:09:35 +0700

    fetch API

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
-async function getAdvice() {
-  const response = await fetch("https://api.adviceslip.com/advice");
-  const data = await response.json();
-  console.log(data.slip.advice);
-}
+const adviceIdEl = document.getElementById("advice-id");
+const adviceParagraphEl = document.getElementById("advice-paragraph");
 
-const advice = getAdvice();
+const generateAdviceBtn = document.getElementById("cta");
+
+generateAdviceBtn.addEventListener("click", () => {
+  getAdvice();
+});
+
+// idk what happened but this API endpoint has a problem with caching
+async function getAdvice() {
+  const response = await fetch("https://api.adviceslip.com/advice", {
+    cache: "no-cache",
+  });
+  const data = await response.json();
+
+  adviceIdEl.innerText = data.slip.id;
+  adviceParagraphEl.innerText = data.slip.advice;
+}
