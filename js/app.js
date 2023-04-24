5acd140177e569f83ce1d0d2ed5a068b2194f438
Efraim Munthe <efrayanglain@gmail.com>
2023-04-24 08:19:15 +0700

    initial fetch script

---
+++ js/app.js
@@ -0,0 +1,7 @@
+async function getAdvice() {
+  const response = await fetch("https://api.adviceslip.com/advice");
+  const data = await response.json();
+  console.log(data.slip.advice);
+}
+
+const advice = getAdvice();
